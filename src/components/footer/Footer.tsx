05afee45454dcacc1bb8128262914fc6222d5ccf
Acdaling <acdaling@gmail.com>
2021-12-13 12:32:08 +0100

    Added the color orange css variable for the footer button
    -

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,18 +14,19 @@ const ResultPageFooterContent = (
    <div className="row">
       <div className="col-12 col-lg-2 offset-lg-10">
          <div className="button-container">
-            <button className="btn footer-button" type="submit">
-               Export
-            </button>
+            <button className="btn footer-button">Export</button>
          </div>
       </div>
    </div>
 );
 
+// Footer content for the assessment page
 const Footer: React.FC<FooterProps> = ({ forPage }) => {
    return (
       <footer className="container-fluid" id="app-footer">
-         {forPage === FOOTER_CONTENT.RESULT ? ResultPageFooterContent : ''}
+         {forPage === FOOTER_CONTENT.RESULT
+            ? ResultPageFooterContent
+            : 'footer for the assessment page'}
       </footer>
    );
 };
