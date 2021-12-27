8298d22cdecad76821793012790734656a3f9def
Acdaling <acdaling@gmail.com>
2021-12-27 14:52:23 +0100

    Adjusted the CurrentQuestionState

--- src/recoil/question/selector/index.ts
+++ src/recoil/question/selector/index.ts
@@ -1,16 +1,35 @@
 import { selector } from 'recoil';
-import { currentQuestionIdState, untypedQuestionState } from '../atom';
+import {
+   ciaQuestionState,
+   currentCiaTypeState,
+   currentQuestionIdState,
+   untypedQuestionState,
+} from '../atom';
 
 export const currentQuestionState = selector({
    key: 'currentQuestion',
    get: ({ get }) => {
       const currentQuestionId = get(currentQuestionIdState);
       const allQuestions = get(untypedQuestionState);
+      const ciaQuestions = get(ciaQuestionState);
+      const currentCiaType = get(currentCiaTypeState);
 
       // Get index of the question that needs to be answered
       const index: number = allQuestions.findIndex(
          (el) => el.id === currentQuestionId
       );
+
+      // If the questions has not been found, look inside the CIA questions
+      if (index === -1) {
+         const currentObject = ciaQuestions.find(
+            (el) => el.cia_type === currentCiaType
+         );
+         const currentQuestion = currentObject?.questions?.find(
+            (el) => el.id === currentQuestionId
+         );
+
+         if (currentQuestion) return currentQuestion;
+      }
 
       return allQuestions[index];
    },
