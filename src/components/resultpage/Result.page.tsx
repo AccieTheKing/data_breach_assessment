808a8cfca4d3adc1cac70f13a4541e7e52f95683
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2022-01-19 12:04:22 +0100

    Development (#27)
    * Code documentation + filter function extended (#25)
    
    Co-authored-by: Acdaling <38664776+AccieTheKing@users.noreply.github.com>
    
    * PDF Functionality (#26)
    
    * Created api functions to get and store assessments
    
    - Storing assessments is working
    - Retrieving all assessments is working
    
    *Note that you will need to have the backend running in order to see this work*
    
    * Assessment history showing database data
    
    When the backend is running and the database has been setup correctly you will be able to see the data stored in the database
    
    * Assessments from database are loaded correctly
    
    - After clicking on the Details button, the answers given to an assessment are loaded correctly
    - Added Regex for the incident number
    - Updated question #4 of simple data
    
    * Some styling applied to some components
    
    - Back to list button in the footer when clicking on details buttons on the history page
    - Added border color for "INSIGNIFICANT" score
    
    * Info button added for each type of question
    
    * Some improvements
    
    - Fixed a spelling mistake
    - Added description for each type of question and linking to the correct page and section for more information about the question
    - Removed some spelling mistakes (Thanks Carlos for pointing a lot out)
    - Added Notes to the Result page
    - Added the question number #20 in the questionaire
    - Adjusted question #4 in the Questionaire (Thanks Hajin for pointing that one out)
    - Added link to the About page from the information button in the questionaire screen.
    - Regex check for the incident number
    
    * Added check for directly navigating to questionnaire
    
    * Renamed "Back to list" to "Back to history"
    
    * Added reset function when navigating to home screen
    
    * it removes all values in the input fields if they contain data
    
    * Changed the regular expression on the homepage
    
    * The historypage the list is now sorted
    
    * Action list works
    
    - Show which action there should be taken based on the Severity level.
    
    * Added question text to the api endpoint as well
    
    * Able to create pdf of made assessments
    
    Co-authored-by: CarlosMadle <73390771+CarlosMadle@users.noreply.github.com>

--- src/components/resultpage/Result.page.tsx
+++ src/components/resultpage/Result.page.tsx
@@ -28,6 +28,37 @@ const ImpactScoreVisual: React.FC<{ title: string; score: number }> = ({ title, 
    );
 };
 
+const ActionList: React.FC<{ SL: string }> = ({ SL }) => {
+   const showActionItem = () => {
+      const conditionNotSupAuthorities =
+         SL === ASSESSMENT_IMPACT_TITLE.HIGH || SL === ASSESSMENT_IMPACT_TITLE.CRITICAL;
+      const conditionComToDataSubjects = SL === ASSESSMENT_IMPACT_TITLE.CRITICAL;
+
+      return {
+         authorities: conditionNotSupAuthorities ? 'list-item' : 'none',
+         dataSubject: conditionComToDataSubjects ? 'list-item' : 'none',
+      };
+   };
+   return (
+      <div className="row">
+         <div className="col-12 col-lg-8 offset-lg-2">
+            <div className="action_list_container">
+               <h2>Action list</h2>
+               <ol className="action_list">
+                  <li>Internal Documentation</li>
+                  <li style={{ display: `${showActionItem().authorities}` }}>
+                     Notify Supervisory Authorities
+                  </li>
+                  <li style={{ display: `${showActionItem().dataSubject}` }}>
+                     Communicate to the data subjects
+                  </li>
+               </ol>
+            </div>
+         </div>
+      </div>
+   );
+};
+
 const Resultpage: React.FC = () => {
    // Set the values of the selected assessment
    const params = useParams<{ id: string }>();
@@ -74,7 +105,11 @@ const Resultpage: React.FC = () => {
             const answers: Array<ICurrentAssessmentAnswers> = assessment.answers!.map((el) => {
                const transfer_answer =
                   el.answer_text === 'true' ? true : el.answer_text === 'false' ? false : el.answer_text;
-               return { id: parseInt(el.question_number), answer: transfer_answer };
+               return {
+                  id: parseInt(el.question_number),
+                  questionText: el.question_text,
+                  answer: transfer_answer,
+               };
             });
 
             setAnswers(answers);
@@ -94,17 +129,8 @@ const Resultpage: React.FC = () => {
       if (params && params.id) {
          onGetAssessment(parseInt(params.id));
       }
-   }, [
-      params,
-      setAnswers,
-      setAssessmentDate,
-      setAssessmentNote,
-      setAssessorData,
-      setImpactNumber,
-      setIncidentNumber,
-      setResultText,
-      title,
-   ]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
    return (
       <>
@@ -136,6 +162,7 @@ const Resultpage: React.FC = () => {
             </div>
          </header>
          <main className="container">
+            <ActionList SL={title} />
             {currentAssessment.notes.length > 0 && (
                <div className="row mb-2">
                   <div className="col-12">
@@ -155,28 +182,6 @@ const Resultpage: React.FC = () => {
             <div className="row">
                <div className="col-12">
                   <InteractiveQuestionary interactive={true} />
-               </div>
-            </div>
-
-            <div className="row">
-               <div className="col-12 col-lg-8 offset-lg-2">
-                  <div className="action_list_container">
-                     <h2>Action list</h2>
-                     <ol className="action_list">
-                        <li>
-                           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae optio ducimus
-                           consequatur ullam aspernatur illum quia
-                        </li>
-                        <li>
-                           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae optio ducimus
-                           consequatur ullam aspernatur illum quia
-                        </li>
-                        <li>
-                           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae optio ducimus
-                           consequatur ullam aspernatur illum quia
-                        </li>
-                     </ol>
-                  </div>
                </div>
             </div>
          </main>
